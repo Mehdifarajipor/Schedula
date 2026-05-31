c3a5cd10bac93be56ea6042ba23094633a293455
mehdi <mehdifarajise666@gmail.com>
2026-05-31 17:25:32 +0330

    configure rest_framework & corsheaders in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'corsheaders',
+
     'schedula_apps.accounts.apps.AccountsConfig',
 ]
 
@@ -49,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
 ]
 
 ROOT_URLCONF = 'config.urls'
