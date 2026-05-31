3d06989e967abbc45703900be9913d387d76f810
mehdi <mehdifarajise666@gmail.com>
2026-05-31 17:12:04 +0330

    initial accounts app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'schedula_apps.accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
