815a142d0ad2d9d0a82c3d9dd1abfbbe1a5e8fc1
mehdi <mehdifarajise666@gmail.com>
2026-06-01 12:12:03 +0330

    link pages(home&login&...) in navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,7 +1,12 @@
+import {Link} from "react-router-dom";
+
 function Navbar(){
     return(
         <nav>
-            <h1>Schedula</h1>
+            <Link to={"/"}> Home </Link>
+            <Link to={"/dashboard"}> Dashboard </Link>
+            <Link to={"/login"}> Login </Link>
+            <Link to={"/register"}> Register </Link>
         </nav>
     );
 }
