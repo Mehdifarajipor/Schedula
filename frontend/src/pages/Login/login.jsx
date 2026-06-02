ca6ce1d5aa50ba7f883622a42ed355d51b677549
mehdi <mehdifarajise666@gmail.com>
2026-06-02 09:33:43 +0330

    feat(auth): implement navigation user from login page to dashboard page

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -1,5 +1,16 @@
+import {useNavigate} from "react-router-dom";
+
 function Login() {
-    return <h1>Login Page</h1>
+    const navigate = useNavigate()
+
+    function handleLogin(){
+        console.log("login was successfully")
+        navigate("/dashboard");
+    }
+
+    return(
+        <button onClick={handleLogin}>Login</button>
+    );
 }
 
 export default Login
