00103acc0038394b38b8f093a351cc6a2fc5f0ee
mehdi <mehdifarajise666@gmail.com>
2026-06-03 15:48:39 +0330

    feat: customize User model

--- backend/Schedula_apps/accounts/models.py
+++ backend/Schedula_apps/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        CUSTOMER = "CUSTOMER", "Customer"
+        PROVIDER = "PROVIDER", "Provider"
+        ADMIN = "ADMIN", "Admin"
+
+    # fields
+    email = models.CharField(max_length=200, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=10, choices=RoleChoices.choices, default=RoleChoices.CUSTOMER)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # settings
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
+
+    # managers
+    objects = UserManager()
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
+
+    def __str__(self):
+        return f"{self.username if self.username else ''}: {self.email}"
