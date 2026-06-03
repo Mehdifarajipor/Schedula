3bdb502d0eb300dd67d0e438fc41baf588fa0e55
mehdi <mehdifarajise666@gmail.com>
2026-06-03 15:53:20 +0330

    chore: add accounts app in  installed_apps & introducting accounts.User to AUTH USER

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 
-    'schedula_apps.accounts.apps.AccountsConfig',
+    'Schedula_apps.accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+#Authentication
+
+AUTH_USER_MODEL = "accounts.User"
