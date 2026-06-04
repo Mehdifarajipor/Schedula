59da57c987e46541d05a78135b40da70bf75fe3a
mehdi <mehdifarajise666@gmail.com>
2026-06-04 08:54:32 +0330

    feat: add ProviderProfile model

--- backend/Schedula_apps/accounts/models.py
+++ backend/Schedula_apps/accounts/models.py
@@ -36,3 +36,55 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username if self.username else ''}: {self.email}"
+
+
+class ProviderProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="provider_profile")
+
+    # --- Business Identity ---
+    business_name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    bio = models.TextField(blank=True)
+
+    # --- Contact ---
+    phone_number = models.CharField(max_length=11, unique=True)
+    email = models.EmailField
+
+    # --- Location ---
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+    # --- Business Settings ---
+    timezone = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=False)
+
+    # --- Booking Settings ---
+    appointment_buffer = models.PositiveIntegerField(default=0)
+    # the distance between the turns(minutes)
+    advance_booking_days = models.PositiveIntegerField(default=30)
+    # to make an appointment a few days ahead(days)
+    cancellation_policy = models.TextField(blank=True)
+
+    # --- Rating / Trust ---
+    rating = models.FloatField(default=0)
+    total_reviews = models.PositiveIntegerField(default=0)
+
+    # --- Media ---
+    logo = models.ImageField(upload_to="providers/logos/", null=True, blank=True)
+
+    # --- System ---
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.business_name
+
+
+
+
+
+
