26846abfc3e5c2e47365da7e08d9c02331013f3e
mehdi <mehdifarajise666@gmail.com>
2026-06-05 11:03:46 +0330

    feat: add login feature by JWT

--- backend/Schedula_apps/accounts/urls.py
+++ backend/Schedula_apps/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 
@@ -7,5 +9,7 @@ app_name = "accounts"
 
 
 urlpatterns = [
-    path('register/', views.RegisterApiView.as_view(), name='register')
+    path('register/', views.RegisterApiView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 ]
