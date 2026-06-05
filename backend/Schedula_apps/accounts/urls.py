9ecff2e8b3b613d2b8560a65bd35865a73926970
mehdi <mehdifarajise666@gmail.com>
2026-06-05 10:46:49 +0330

    feat: add url for register

---
+++ backend/Schedula_apps/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from . import views
+
+
+app_name = "accounts"
+
+
+urlpatterns = [
+    path('register/', views.RegisterApiView.as_view(), name='register')
+]
