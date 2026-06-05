5af7d18bba53934f98ebbe415d02eaecf44bc7e5
mehdi <mehdifarajise666@gmail.com>
2026-06-05 10:44:18 +0330

    feat:add serializer for register

---
+++ backend/Schedula_apps/accounts/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import password_validation
+
+from rest_framework import serializers
+
+from .services import UserServices
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    phone_number = serializers.CharField(max_length=11, min_length=11)
+    username = serializers.CharField(max_length=250)
+    password = serializers.CharField(write_only=True, min_length=8, max_length=250)
+
+    class Meta:
+        model = User
+        fields = ['email', 'phone_number', 'username', 'password']
+
+    def validate_password(self, value):
+        password_validation.validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        return UserServices.create_user(**validated_data)
