8c285e163eb3989fd762b09811dff40dccc10140
mehdi <mehdifarajise666@gmail.com>
2026-06-06 11:47:35 +0330

    feat: add APIView for user profile

--- backend/Schedula_apps/accounts/views.py
+++ backend/Schedula_apps/accounts/views.py
@@ -2,10 +2,12 @@ import rest_framework.permissions
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserProfileSerializer
+from .models import User
 
 # Create your views here.
 
@@ -24,3 +26,11 @@ class RegisterApiView(APIView):
             },
             status=status.HTTP_201_CREATED,
         )
+
+
+class UserProfileAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [rest_framework.permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
