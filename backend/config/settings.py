cc4265f631d850aae51a02d02b30403649c9dd14
mehdi <mehdifarajise666@gmail.com>
2026-06-06 11:45:21 +0330

    fix: alter JWT access and refresh token life time

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,5 +152,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=30),
 }
