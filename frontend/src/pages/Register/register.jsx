3d9e95dba7e99587bce77bbb0b0c76c992537d21
mehdi <mehdifarajise666@gmail.com>
2026-06-09 18:23:14 +0330

    feat: add register form

--- frontend/src/pages/Register/register.jsx
+++ frontend/src/pages/Register/register.jsx
@@ -1,5 +1,77 @@
+import {useState} from "react";
+import {register} from "../../services/authApi.js";
+import {useNavigate} from "react-router-dom";
+
+
 function Register() {
-    return <h1>Register Page</h1>
+    const [email, setEmail] = useState("")
+    const [phone_number, setPhone_number] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    let navigate = useNavigate()
+    async function handleSubmit(event){
+        event.preventDefault()
+        localStorage.clear()
+        try{
+            const response = await register({email, phone_number, username, password})
+            console.log(response)
+            navigate("/login")
+        }catch(err){
+            console.log(err.response?.data)
+            console.log(err.response?.status)
+        }
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email</label>
+                    <input
+                        type={"text"}
+                        value={email}
+                        onChange={(element) => {
+                            setEmail(element.target.value)
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <label>Phone number</label>
+                    <input
+                        type={"text"}
+                        value={phone_number}
+                        onChange={(element) => {
+                            setPhone_number(element.target.value)
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <label>Username</label>
+                    <input
+                        type={"text"}
+                        value={username}
+                        onChange={(element) =>{
+                            setUsername(element.target.value)
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input
+                        type={"text"}
+                        value={password}
+                        onChange={(element) =>{
+                            setPassword(element.target.value)
+                        }}
+                    />
+                </div>
+
+                <button type={"submit"}>Register</button>
+            </form>
+        </>
+    );
 }
 
 export default Register
