31cbec6a2c90efcf28a9f1879a78c268f046fe94
mehdi <mehdifarajise666@gmail.com>
2026-06-09 15:32:51 +0330

    link profile page in navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar(){
             <Link to={"/dashboard"}> Dashboard </Link>
             <Link to={"/login"}> Login </Link>
             <Link to={"/register"}> Register </Link>
+            <Link to={"/profile"}>Profile</Link>
         </nav>
     );
 }
