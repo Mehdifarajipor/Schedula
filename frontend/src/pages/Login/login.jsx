a5632b93b360a5833c3fcb188f851f92d105e41e
mehdi <mehdifarajise666@gmail.com>
2026-06-09 15:30:15 +0330

    feat: create form for login

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -1,15 +1,48 @@
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {login} from "../../services/authApi.js";
 
 function Login() {
     const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    function handleLogin(){
-        console.log("login was successfully")
-        navigate("/dashboard");
+    async function handleLogin(event){
+        event.preventDefault()
+        try{
+            const data = await login({email, password})
+            localStorage.setItem('access', data.access)
+            localStorage.setItem('refresh', data.refresh)
+            console.log("login Successful")
+            navigate("/profile")
+        }catch(error){
+            console.log(error)
+        }
     }
 
     return(
-        <button onClick={handleLogin}>Login</button>
+        <form onSubmit={handleLogin}>
+            <div>
+                <label>Email</label>
+                <input type={'text'}
+                       value={email}
+                       onChange={(e) => {
+                           setEmail(e.target.value)
+                       }}
+                />
+            </div>
+            <div>
+                <label>Password</label>
+                <input
+                    type={'text'}
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}
+                />
+            </div>
+            <button type={'submit'}>Login</button>
+        </form>
     );
 }
 
