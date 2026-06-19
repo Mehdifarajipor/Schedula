90b07fe191d06eb8f5571e94462ae1275bf84341
mehdi <mehdifarajise666@gmail.com>
2026-06-19 09:20:23 +0330

    feat: add serializer for password change feature

--- backend/Schedula_apps/accounts/serializers.py
+++ backend/Schedula_apps/accounts/serializers.py
@@ -32,3 +32,33 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'id',
             'email'
         )
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    confirm_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if user.check_password(value):
+            return value
+        raise serializers.ValidationError({'old_password': 'your password is not valid!!!'})
+
+    def validate_new_password(self, value):
+        user = self.context['request'].user
+        password_validation.validate_password(value, user)
+        return value
+
+    def validate(self, data):
+        if data['confirm_password'] != data['new_password']:
+            raise serializers.ValidationError({'confirm_password': 'the passwords do not match!!!'})
+        if data['new_password'] == data['old_password']:
+            raise serializers.ValidationError({'new_password': 'your password can not be same like your old password'})
+        return data
+
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save(update_fields=['password'])
+        return user
