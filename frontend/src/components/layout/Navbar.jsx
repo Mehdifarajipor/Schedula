f34e9bc0be5cf6f4dba56480c975f2479f1c3fec
mehdi <mehdifarajise666@gmail.com>
2026-07-08 10:32:18 +0330

    feat: show profile link only for authenticated users

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,13 +1,31 @@
 import {Link} from "react-router-dom";
+import api from "../../services/api.js";
+import {useEffect, useState} from "react";
 
 function Navbar(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+useEffect(() => {
+    async function checkLogin() {
+        try {
+            await api.get("accounts/me/");
+            setIsLoggedIn(true);
+        } catch {
+            setIsLoggedIn(false);
+        }
+    }
+
+    checkLogin();
+}, []);
     return(
         <nav>
             <Link to={"/"}> Home </Link>
             <Link to={"/dashboard"}> Dashboard </Link>
-            <Link to={"/login"}> Login </Link>
             <Link to={"/register"}> Register </Link>
-            <Link to={"/profile"}>Profile</Link>
+            {isLoggedIn ?
+                (<Link to="/profile">Profile</Link>) :
+                (<Link to="/login">Login</Link>)
+            }
         </nav>
     );
 }
