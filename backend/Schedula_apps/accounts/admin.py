a144ee4dccab1caa4510de04691b3b8f9879cca6
mehdi <mehdifarajise666@gmail.com>
2026-07-25 08:45:20 +0330

    feat: register ProviderProfile model in admin

--- backend/Schedula_apps/accounts/admin.py
+++ backend/Schedula_apps/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, ProviderProfile
 
 # Register your models here.
 
@@ -8,3 +8,9 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['email', 'username', 'phone_number', 'role', 'date_joined']
+
+
+@admin.register(ProviderProfile)
+class ProviderProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'business_name', 'email']
+    prepopulated_fields = {'slug': ['business_name']}
