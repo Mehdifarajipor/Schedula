9ff7f0b9fcb915ff87679c696a834ea5cfec04be
mehdi <mehdifarajise666@gmail.com>
2026-07-29 17:11:36 +0330

    feat: add Service and Category model

--- backend/Schedula_apps/services/admin.py
+++ backend/Schedula_apps/services/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Category, Service, ServiceImage
+
 # Register your models here.
+
+class ServiceImageInline(admin.TabularInline):
+    model = ServiceImage
+    extra = 0
+
+
+@@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'provider', 'category']
--- backend/Schedula_apps/services/models.py
+++ backend/Schedula_apps/services/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
+from ..accounts.models import ProviderProfile
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    provider = models.ForeignKey(ProviderProfile, models.CASCADE, "services")
+    category = models.ForeignKey(Category, models.SET_NULL, "services", null=True)
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.PositiveIntegerField(help_text="minutes")
+
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class ServiceImage(models.Model):
+    service = models.ForeignKey(Service, models.CASCADE, "images")
+    file = models.ImageField(upload_to="services/")
